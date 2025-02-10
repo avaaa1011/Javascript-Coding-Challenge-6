7efe05800382ec5e7d7fed844dc121825737c163
Ava Tran <ava31@usf.edu>
2025-02-09 23:41:48 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -17,3 +17,16 @@ function calculateSalesTax(amount, taxRate) //declaring a function that calculat
 calculateSalesTax(100, 0.07); //test data that should produce $7
 calculateSalesTax(500, 0.1); //test data that should produce $50
 
+//Task 3 - Employee Bonus Calculation
+//arrow function 
+const calculateBonus = (salary, performanceRating) => 
+{
+    if (performanceRating === "Excellent"){ //if employee's perfomance is excellent then they will receive a 20% bonus worth of their salary
+        bonus = salary * 0.20
+    } else if (performanceRating === "Good"){ //if employee's perfomance is good then they will receive a 10% bonus worth of their salary
+    } else if (performanceRating === "Average"){ //if employee's perfomance is average then they will receive a 5% bonus worth of their salary
+    };
+    console.log(`Employee Bonus: $${bonus}`);
+}
+calculateBonus(5000, "Excellent"); //test data should should produce expected bonus of $1000
+calculateBonus(7000, "Good"); //test data should produce expected bonus of $700
