9f8ce36fb02b0587811113585981345c5abfb3ea
Ava Tran <ava31@usf.edu>
2025-02-09 23:26:38 -0500

    Task 2 - Sales Tax Conputation

--- cc_6.js
+++ cc_6.js
@@ -7,3 +7,13 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) //declaring a funct
     };
 calculateProfit(20, 30, 100); //test data that should produce $1000
 calculateProfit(50, 70, 200); //test data that should produce $4000
+
+//Task 2 - Sales Tax Computation
+function calculateSalesTax(amount, taxRate) //declaring a function that calculate sales tax
+    {
+        let salesTax = amount*taxRate;
+        console.log(`Sales Tax: $${Math.floor(salesTax)}`); 
+    };
+calculateSalesTax(100, 0.07); //test data that should produce $7
+calculateSalesTax(500, 0.1); //test data that should produce $50
+
